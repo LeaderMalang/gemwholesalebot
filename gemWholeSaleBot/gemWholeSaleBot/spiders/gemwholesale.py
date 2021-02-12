4c2d72f1d1b46ef5f5043ede47bce27f7e7ea8eb
hassan ali <hassanali5120@gmail.com>
2021-02-12 23:15:45 +0500

    written fews lines for product scrapping

--- gemWholeSaleBot/gemWholeSaleBot/spiders/gemwholesale.py
+++ gemWholeSaleBot/gemWholeSaleBot/spiders/gemwholesale.py
@@ -4,7 +4,14 @@ import scrapy
 class GemwholesaleSpider(scrapy.Spider):
     name = 'gemwholesale'
     allowed_domains = ['gemwholesale.co.uk']
-    start_urls = ['https://gemwholesale.co.uk/']
+    start_urls = ['https://www.gemwholesale.co.uk/acatalog/Customer-Returns.html']
 
     def parse(self, response):
-        pass
+        productContainer=response.xpath('//div[@class="product-details"]').getall()
+        for product in productContainer:
+            title=product.xpath('//a[@target="ActPopup"]/h2::text()').get()
+            image=product.xpath('//p[@class="product-image"]/a/img/@src').get()
+            cost_price=product.xpath('//span[@class="product-price"]/span::text()').get()
+            catalogue_value=product.xpath("//table[@id='stockdetails']/tbody/tr[1]/td[2]/span::text()").get()
+
+        print(response)
