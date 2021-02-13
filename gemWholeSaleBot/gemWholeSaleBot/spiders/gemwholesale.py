84e59dace84d94f56570c2ac7f02ef3c1f2add49
hassanalituf <web.developer@tuf.edu.pk>
2021-02-13 17:48:26 +0500

    created entry point for the script and also scrapped one page parsing values is pending

--- gemWholeSaleBot/gemWholeSaleBot/spiders/gemwholesale.py
+++ gemWholeSaleBot/gemWholeSaleBot/spiders/gemwholesale.py
@@ -7,11 +7,15 @@ class GemwholesaleSpider(scrapy.Spider):
     start_urls = ['https://www.gemwholesale.co.uk/acatalog/Customer-Returns.html']
 
     def parse(self, response):
-        productContainer=response.xpath('//div[@class="product-details"]').getall()
+        productContainer=response.xpath('//div[@class="product-details"]')
         for product in productContainer:
-            title=product.xpath('//a[@target="ActPopup"]/h2::text()').get()
-            image=product.xpath('//p[@class="product-image"]/a/img/@src').get()
-            cost_price=product.xpath('//span[@class="product-price"]/span::text()').get()
-            catalogue_value=product.xpath("//table[@id='stockdetails']/tbody/tr[1]/td[2]/span::text()").get()
+            print(product)
+            title=product.xpath('//div[@class="product-details"]/div/table/a[2]/h2/text()').get()
+            image=product.css(".product-image").xpath('//p/a/img/@src').get()
+            cost_price=product.xpath('table[1]/tr[3]/td[2]/p/prices/div/span/span/text()').get()
+            catalogue_value=product.xpath('table[1]/tr[1]/td[2]/span/text()').get()
+            sold_at=product.xpath('table[1]/tr[2]/td[2]/span/text()').get()
+            stock_list=product.css('.product-text a::attr(href)').get()
+            print(title,image,cost_price,catalogue_value,sold_at,stock_list)
 
         print(response)
